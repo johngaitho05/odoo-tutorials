183046b976f25e329f169b730abf25723b60f46e
John Gaitho <johngaitho05@gmail.com>
2025-04-18 17:16:36 +0300

    [IMP] awesome_dashboard: make it generic

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -7,17 +7,17 @@ import {ControlPanel} from "@web/search/control_panel/control_panel";
 import { useService } from "@web/core/utils/hooks";
 import {_t} from "@web/core/l10n/translation";
 import { DashboardItem } from './dashboard_item'
-import { PieChart  } from './pie_chart'
+import { items } from './dashboard_items'
 
 export class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, ControlPanel, DashboardItem, PieChart }
+    static components = { Layout, ControlPanel, DashboardItem }
 
     setup() {
         this.action = useService('action')
         const dataService = useService('statistics')
-        this.data = useState(dataService.statistics);
-        console.log("dashboard loaded")
+        this.statistics = useState(dataService.statistics);
+        this.items = items
     }
 
     openCustomers(){
